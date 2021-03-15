650e8485fbb50537b5185bcf3d8af8a867810942
Giorgio <giorgio.pignattari@gmail.it>
2021-03-15 15:57:02 +0100

    html e css ok, inizio js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Consegna
+// La pagina espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
+// tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri,
+//  il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// genero 5 numeri random e li mostro all'utente
