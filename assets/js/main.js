073a540811fc2d64a82d7529207a581ada5fd200
Giorgio <giorgio.pignattari@gmail.it>
2021-03-15 17:32:10 +0100

    numeri stampati

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 // Consegna
 // La pagina espone 5 numeri generati casualmente.
-// Da li parte un timer di 30 secondi.
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
+// Da li parte un timer di 10 secondi.
+// Dopo 10 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
 // tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri,
 //  il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
-// genero 5 numeri random da 1 a 100 e li mostro all'utente
 // array vuoto per i numeri generati
 var randomNumbers = [];
+// genero 5 numeri random da 1 a 100 e li mostro all'utente
 randomNumberPc(1, 100);
 numberRange(randomNumbers, 100);
-// e stampo il risultato
+// e stampo il risultato anche su HTML
 console.log("I numeri generati sono: " + randomNumbers);
-
+$("#primo").text(randomNumbers[0]);
+$("#secondo").text(randomNumbers[1]);
+$("#terzo").text(randomNumbers[2]);
+$("#quarto").text(randomNumbers[3]);
+$("#quinto").text(randomNumbers[4]);
 
 
 
