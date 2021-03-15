beb6ea51e7d0bfdc4139bfb8db06caf0b52cb6a7
Giorgio <giorgio.pignattari@gmail.it>
2021-03-15 16:21:54 +0100

    generati 5 numeri random

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,30 @@
 // Dopo che sono stati inseriti i 5 numeri,
 //  il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// genero 5 numeri random e li mostro all'utente
+
+// genero 5 numeri random da 1 a 100 e li mostro all'utente
+// array vuoto per i numeri generati
+var randomNumbers = [];
+randomNumberPc(1, 100);
+numberRange(randomNumbers, 100);
+// e stampo il risultato
+console.log("I numeri generati sono: " + randomNumbers);
+
+
+
+
+// FUNZIONI
+// numeri random
+function randomNumberPc(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// prendo 5 numeri dai 100 generati random
+function numberRange(array, max) {
+  while (array.length < 5) {
+    var numero = randomNumberPc(1, max);
+    if (!array.includes(numero)) {
+      array.push(numero);
+    }
+  }
+}
